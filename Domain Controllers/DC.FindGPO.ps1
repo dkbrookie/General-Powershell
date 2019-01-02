98f301b90e3ebeb25150535bf71d0f9b310a4fb0
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-01-02 15:38:39 -0600

    Update DC.FindGPO.ps1

--- Domain Controllers/DC.FindGPO.ps1
+++ Domain Controllers/DC.FindGPO.ps1
@@ -9,12 +9,28 @@ write-host "Finding all the GPOs in $DomainName"
 Import-Module grouppolicy
 $allGposInDomain = Get-GPO -All -Domain $DomainName
 ## Look through each GPO's XML for the string
-Write-Host "Starting search...."
+Write-Host "Searching...."
 ForEach ($gpo in $allGposInDomain) {
   $report = Get-GPOReport -Guid $gpo.Id -ReportType Xml
   If ($report -match $string) {
-    write-host "********** Match found in: $($gpo.DisplayName) **********"
-  } Else {
-    Write-Host "No match in: $($gpo.DisplayName)"
+    $report | Out-File "c:\gpoOut.txt"
+    $report = Get-Content "c:\gpoOut.txt"
+    Write-Host "**************************************"
+    Write-Host "** Match found in GPO"
+    Write-Host "    $($gpo.DisplayName)"
+    $valuePattern = "<q1:Value>(.*)</q1:Value>"
+    $value = [regex]::match($report, $valuePattern).Groups[1].Value
+    $linkPattern = "<SOMPath>(.*)</SOMPath>"
+    $link = $report | Select-String -Pattern $linkPattern
+    $link = $link -replace("<SOMPath>","") -replace ("</SOMPath>?","")
+    Write-Host "** Current Linked OUs"
+    ForEach($ou in $link) {
+        $ou
+    }
+    Write-Host "**************************************"
   }
+}
+
+If((Test-Path C:\gpoOut.txt -PathType Leaf)) {
+    Remove-Item -Path C:\gpoOut.txt -Force
 }
