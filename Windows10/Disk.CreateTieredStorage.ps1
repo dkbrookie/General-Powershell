832ed5591555e3836a4723f07a45ad326c240c5d
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-01-01 22:13:31 -0600

    Update Disk.CreateTieredStorage.ps1

--- Windows10/Disk.CreateTieredStorage.ps1
+++ Windows10/Disk.CreateTieredStorage.ps1
@@ -6,6 +6,7 @@ If(!$physDisks) {
 }
 
 Write-Warning "All of the disks listed will be put into your new tiered storage volume and have their data wipe! Proceed with caution!"
+$physDisks
 $answer = Read-Host "Are you sure you want to create tiered storage out of all the drives just listed? (y/n)"
 If($answer -eq "y") {
   $ssdSize = Read-Host "How many GBs do you want your SSD pool to be? (enter number only)"
@@ -13,14 +14,10 @@ If($answer -eq "y") {
   If(!$ssdSize) {
     Write-Warning "You have to enter a number for your total SSD size for this script to proceed! Exiting script."
     Return
-  } Else {
-    $ssdSize = (-Join($ssdSize,"GBs"))
   }
   If(!$hddSize) {
     Write-Warning "You have to enter a number for your total HDD size for this script to proceed! Exiting script."
     Return
-  } Else {
-    $hddSize = (-Join($hddSize,"GBs"))
   }
   ## Store all physical disks that can be pooled into a variable, $pd
   $pd = (Get-PhysicalDisk -CanPool $True | Where MediaType -NE UnSpecified)
@@ -34,9 +31,8 @@ If($answer -eq "y") {
   $ssd_Tier = New-StorageTier -StoragePoolFriendlyName “Optane Boosted Storage” -FriendlyName SSD_Tier -MediaType SSD
   $hdd_Tier = New-StorageTier -StoragePoolFriendlyName “Optane Boosted Storage” -FriendlyName HDD_Tier -MediaType HDD
 
-
   ## Create a new virtual disk in the pool with a name of TieredSpace using the SSD (50GB) and HDD (300GB) tiers
-  $vd1 = New-VirtualDisk -StoragePoolFriendlyName “Optane Boosted Storage” -FriendlyName TieredSpace -StorageTiers @($ssd_tier, $hdd_tier) -StorageTierSizes @($ssdSize, $hddSize) -ResiliencySettingName Simple -WriteCacheSize 1GB
+  $vd1 = New-VirtualDisk -StoragePoolFriendlyName “Optane Boosted Storage” -FriendlyName TieredSpace -StorageTiers @($ssd_tier, $hdd_tier) -StorageTierSizes @(52GB, 9310GB) -ResiliencySettingName Simple -WriteCacheSize 1GB
 } Else {
   Write-Output "Exiting script"
 }
