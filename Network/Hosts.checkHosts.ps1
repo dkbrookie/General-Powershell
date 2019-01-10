61597cd88e489aa1a34007315df8e97495bc5811
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-01-10 12:34:56 -0600

    Added hosts check

---
+++ Network/Hosts.checkHosts.ps1
@@ -0,0 +1,21 @@
+$hosts = "$env:windir\System32\drivers\etc\hosts"
+$hostsBackup = "$env:windir\LTsvc\hosts.bak"
+
+Try {
+  $hostsContent = Get-Content $hosts
+  $rowCount = $hostsContent.Count
+
+  If($rowCount -gt 21) {
+    Write-Host "Hosts file has been altered!"
+    If(!(Test-Path $hostsBackup)) {
+      Copy-Item $hosts -Destination $hostsBackup
+      Write-Output "Created a backup of your hosts file at $hostsBackup"
+    } Else {
+      Write-Output "Verified there is already a backup of the hosts file at $hostsBackup"
+    }
+  } Else  {
+    Write-Host "Default hosts file confirmed"
+  }
+} Catch {
+  Write-Error "Encountered an error while trying to verify the hosts file"
+}
