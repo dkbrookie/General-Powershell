b2ffc84d0e9220998fe88d256d8135499994e5a5
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-02-01 08:57:19 -0600

    Update Security.DisableLocalUsers.ps1
    Added detailed notes to the script and added a computer shutdown at the end.

--- Security/Security.DisableLocalUsers.ps1
+++ Security/Security.DisableLocalUsers.ps1
@@ -1,3 +1,19 @@
+<#
+.DESCRIPTION
+This script is meant for a machine tha tneeds to be completely locked out. This
+is generally used for a remote termination to make sure the employee cannot
+access company files / systems effective immediately, in case they don't connect
+to VPN.
+
+Steps
+1) Disables all local user accounts
+2) Disables cached credentials for domain accounts-- meaning the machine can no
+longer login unless you get on VPN and validate your credentials against the DC.
+Assuming the account has now been disabled, that validation would fail.
+3) Force logs out all users on the machine
+4) Shuts down computer
+
+#>
 ## Set vars
 $registryPath = 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon'
 $Name = 'CachedLogonsCount'
@@ -16,12 +32,11 @@ ForEach($user in $lUsers) {
     }
 }
 
-
 ## Clear all cached credentials via Registry
 Set-ItemProperty -Path $registryPath -Name $name -Value $value
-
 
 ## Log off all users from the machine
 (gwmi win32_operatingsystem).Win32Shutdown(4)
 
+## Shutdown computer
 Stop-Computer
